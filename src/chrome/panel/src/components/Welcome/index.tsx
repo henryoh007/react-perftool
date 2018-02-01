55df8a9e4b464d60b523bd54d2087b517495505f
Vahe <martinkenner@mail.com>
2018-02-01 16:05:16 +0400

    -0,0 +1

---
+++ src/chrome/panel/src/components/Welcome/index.tsx
@@ -0,0 +1,19 @@
+// <Welcome/>
+import * as React from "react";
+import "./index.scss";
+
+export default function Welcome(props) {
+	const Logo = chrome.extension.getURL("./panel/src/res/icons/react-logo.svg");
+	return (
+		<div className="react-perftool-container">
+			<div className="align-center">
+				<img src={`${Logo}`} width="100" height="100" />
+				<div>
+					<p>Welcome to React Performance tab.</p>
+					<p>Inspect the performance of React Js components.</p>
+					<p>For more detauls got to github page</p>
+				</div>
+			</div>
+		</div>
+	);
+}
