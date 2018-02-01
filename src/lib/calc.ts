d3ea916e6c7061f1c4a7011dc9e28c5f6be86f75
Vahe <martinkenner@mail.com>
2018-02-01 16:11:09 +0400

    -0,0 +1

---
+++ src/lib/calc.ts
@@ -0,0 +1,26 @@
+// Calc
+export function unsetAll(name: string) {
+	window.perfTool = {
+		...window.perfTool,
+		[name]: {
+			...window.perfTool[name],
+			["Initial Mount"]: 0,
+			["Update Time"]: 0,
+			["Re-rendered"]: 0
+		}
+	};
+}
+
+export function setup(name: string, result: number | boolean, prop: string) {
+	window.perfTool = {
+		...window.perfTool,
+		[name]: {
+			...window.perfTool[name],
+			[prop]: result
+		}
+	};
+}
+
+export function calc(t1: any, t0: any): number {
+	return parseFloat((t1 - t0).toFixed(4));
+}
