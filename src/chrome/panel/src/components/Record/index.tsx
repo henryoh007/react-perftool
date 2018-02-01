0692174c3cb008107260c0eba862740dc18e8a7d
Vahe <martinkenner@mail.com>
2018-02-01 16:06:13 +0400

    -0,0 +1  -28 +29,3

---
+++ src/chrome/panel/src/components/Record/index.tsx
@@ -0,0 +1,40 @@
+// <Rectod/>
+import * as React from "react";
+import * as Popover from "react-awesome-popover";
+import { inject, observer } from "mobx-react";
+
+import "react-awesome-popover/dest/react-awesome-popover.css";
+
+const Record = ({ monitoring, liveMonitoring, record }) => {
+	function fn() {
+		if (liveMonitoring !== !liveMonitoring) {
+			if (!liveMonitoring) {
+				record();
+			}
+		}
+	}
+	return (
+		<Popover placement="bottom-start" action="hover" arrow={false}>
+			<div onClick={fn}>
+				{monitoring ? (
+					<img
+						width="18.50"
+						height="18.50"
+						src={`${chrome.extension.getURL("./panel/src/res/icons/record_red.svg")}`}
+					/>
+				) : (
+					<img
+						width="18.50"
+						height="18.50"
+						src={`${chrome.extension.getURL(
+							"./panel/src/res/icons/ic_fiber_manual_record_black_24px.svg"
+						)}`}
+					/>
+				)}
+			</div>
+			<p>Record</p>
+		</Popover>
+	);
+};
+
+export default Record;
