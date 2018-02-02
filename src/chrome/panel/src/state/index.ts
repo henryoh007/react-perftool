5e13ecaa5b78cab6f749d11041e663b66b4c1036
Vahe <martinkenner@mail.com>
2018-02-02 18:10:55 +0400

    -148,2 +148,11

--- src/chrome/panel/src/state/index.ts
+++ src/chrome/panel/src/state/index.ts
@@ -145,8 +145,17 @@ class AppState {
 						...{ name: value }
 					})
 				);
-				self.data.length = 0;
-				self.data = arr;
+				if(self.sortBy){
+						if(self.sortBy == 'name' || self.sortBy == 'Re-rendered'){
+						self.data = orderBy(arr,self.sortBy,[self.direction]);
+					} else if(self.sortBy == 'Initial Mount' || self.sortBy == "Update Time"){
+						self.data  = orderBy(arr,item=>parseInt(item[self.sortBy].slice(0,-3)),self.direction);
+					} else {
+						self.data  = self.sortByBool(self.direction);
+					}
+				} else {
+					self.data = arr;	
+				}
 			}
 		});
 	}
