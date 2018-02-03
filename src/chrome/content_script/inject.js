450f6ebd79939aabd227117f9b7340a8430e9161
Vahe <martinkenner@mail.com>
2018-02-03 13:37:58 +0400

    -2,0 +3,43  -16,41 +59,2  -60 +64

--- src/chrome/content_script/inject.js
+++ src/chrome/content_script/inject.js
@@ -1,5 +1,48 @@
 // Inject
 
+function message(event) {
+	if (event.source !== window) {
+		return;
+	}
+
+	var message = event.data;
+
+	if (message.source != "react-perfTool") {
+		return false;
+	}
+
+	if (message.req === "Can start posting me") {
+		window.perfTool.options.isDevPanelOpened = true;
+	}
+
+	if (message.req === "Stop posting me") {
+		window.perfTool.options.isDevPanelOpened = false;
+	}
+	if (message.req === "Can start posting me::post") {
+		window.perfTool.options.isDevPanelOpened = true;
+		doo();
+	}
+	if (message.req === "Prepeare and start for monitoring") {
+		alert("prep");
+		Object.keys(window.perfTool).map((value, index) => {
+			window.perfTool[value]["Re-rendered"] = 0;
+			window.perfTool[value]["Update Time"] = 0;
+			window.perfTool[value]["All lists updated"] = 0;
+		});
+	}
+	if (message.req === "Monitoring ended") {
+		window.postMessage(
+			{
+				data: window.perfTool,
+				req: "Monitoring finished, sending the performance data to devPanel",
+				from: "inject script",
+				source: "react-perfTool"
+			},
+			"*"
+		);
+	}
+}
+
 function fn() {
 	if ("perfTool" in window) {
 		window.doo = function() {
@@ -13,48 +56,9 @@ function fn() {
 				"*"
 			);
 		};
-
-		window.addEventListener("message", function(event) {
-			if (event.source !== window) {
-				return;
-			}
-
-			var message = event.data;
-			if (message.source != "react-perfTool") {
-				return false;
-			}
-
-			if (message.req === "Can start posting me") {
-				window.perfTool.options.isDevPanelOpened = true;
-			}
-
-			if (message.req === "Stop posting me") {
-				window.perfTool.options.isDevPanelOpened = false;
-			}
-			if (message.req === "Can start posting me::post") {
-				window.perfTool.options.isDevPanelOpened = true;
-				doo();
-			}
-			if (message.req === "Prepeare and start for monitoring") {
-				Object.keys(window.perfTool).map((value, index) => {
-					window.perfTool[value]["Re-rendered"] = 0;
-					window.perfTool[value]["Update Time"] = 0;
-					window.perfTool[value]["All lists updated"] = 0;
-				});
-			}
-			if (message.req === "Monitoring ended") {
-				window.postMessage(
-					{
-						data: window.perfTool,
-						req: "Monitoring finished, sending the performance data to devPanel",
-						from: "inject script",
-						source: "react-perfTool"
-					},
-					"*"
-				);
-			}
-		});
+		console.log("hha");
+		window.addEventListener("message", message);
 	}
 }
 
-window.addEventListener("DOMContentLoaded", fn);
+window.addEventListener("load", fn);
